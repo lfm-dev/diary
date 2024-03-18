01e2d3a9f2d914897f50e5fa13e5b4face7d9a1a
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-03-18 18:19:20 -0300

    moving funcs out of new_entry()

--- src/new_entry.py
+++ src/new_entry.py
@@ -4,43 +4,43 @@ import datetime
 from diary import DIARY_DIR_PATH, USR_ARGS
 from utils import launch_text_editor
 
-def new_entry():
-    def chdir_to_entry_year_dir(entry_year):
-        if not os.path.isdir(os.path.join(DIARY_DIR_PATH, entry_year)):
-            os.mkdir(entry_year)
-        os.chdir(entry_year)
+def chdir_to_entry_year_dir(entry_year):
+    if not os.path.isdir(os.path.join(DIARY_DIR_PATH, entry_year)):
+        os.mkdir(entry_year)
+    os.chdir(entry_year)
 
-    def get_entry_title():
-        new_entry_title = ''
-        with open('tmp.md', encoding='utf-8') as new_entry_file:
-            for line in new_entry_file:
-                if line.startswith('#'):
-                    new_entry_title = line.strip('#').strip()
-                    new_entry_title = ('_').join(new_entry_title.split()) # remove extra spaces between words
-                    break
-        return new_entry_title
+def get_entry_title():
+    new_entry_title = ''
+    with open('tmp.md', encoding='utf-8') as new_entry_file:
+        for line in new_entry_file:
+            if line.startswith('#'):
+                new_entry_title = line.strip('#').strip()
+                new_entry_title = ('_').join(new_entry_title.split()) # remove extra spaces between words
+                break
+    return new_entry_title
 
-    def get_entry_date():
-        if USR_ARGS.date:
-            if USR_ARGS.date == 'yesterday':
-                entry_date = datetime.datetime.now() - datetime.timedelta(days=1)
-
-            else:
-                try:
-                    entry_date = datetime.datetime.strptime(USR_ARGS.date, '%d-%m-%y')
-                except ValueError:
-                    print('wrong date format, use %d-%m-%y (e.g. 01-01-24)')
-                    sys.exit(1)
+def get_entry_date():
+    if USR_ARGS.date:
+        if USR_ARGS.date == 'yesterday':
+            entry_date = datetime.datetime.now() - datetime.timedelta(days=1)
 
         else:
-            entry_date = datetime.datetime.now()
+            try:
+                entry_date = datetime.datetime.strptime(USR_ARGS.date, '%d-%m-%y')
+            except ValueError:
+                print('wrong date format, use %d-%m-%y (e.g. 01-01-24)')
+                sys.exit(1)
 
-        year = str(entry_date.year)[-2:] # only last two digits
-        month = str(entry_date.month) if len(str(entry_date.month)) == 2 else f'0{entry_date.month}' # so it always has two digits
-        day = str(entry_date.day) if len(str(entry_date.day)) == 2 else f'0{entry_date.day}' # so it always has two digits
+    else:
+        entry_date = datetime.datetime.now()
 
-        return f'{year}-{month}-{day}', str(entry_date.year)
+    year = str(entry_date.year)[-2:] # only last two digits
+    month = str(entry_date.month) if len(str(entry_date.month)) == 2 else f'0{entry_date.month}' # so it always has two digits
+    day = str(entry_date.day) if len(str(entry_date.day)) == 2 else f'0{entry_date.day}' # so it always has two digits
 
+    return f'{year}-{month}-{day}', str(entry_date.year)
+
+def new_entry():
     entry_date, year = get_entry_date()
     chdir_to_entry_year_dir(year)
     launch_text_editor('tmp.md')
