3364f2e40e157dfaa4be443b96d1a98180b7f650
LFM <146396539+lfm-dev@users.noreply.github.com>
2024-03-21 15:07:41 -0300

    show help if no flags are used

--- src/diary.py
+++ src/diary.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
 import os
 from new_entry import new_entry
-from get_usr_input import USR_ARGS
+from get_usr_input import USR_ARGS, PARSER
 from settings import SETTINGS
 
 #TODO export entry as pdf with pandoc: pandoc entry_name.md --pdf-engine=xelatex -o entry_name.pdf
 
 def main():
     os.chdir(SETTINGS['diary_path'])
+
     if USR_ARGS.new_entry:
         new_entry()
+
+    else:
+        PARSER.print_help()
 
 if __name__ == '__main__':
     main()
--- src/get_usr_input.py
+++ src/get_usr_input.py
@@ -3,7 +3,7 @@ import argparse
 def get_args():
     parser = argparse.ArgumentParser(prog='diary', description='Manage your digital diary')
     parser.add_argument('-e', '-entry', action='store_true', dest='new_entry', help='write new entry')
-    parser.add_argument('-d', '-date', metavar='', dest='date', help='change date of writing')
+    parser.add_argument('-d', '-date', metavar='', dest='date', help='change date of new entry')
     args = parser.parse_args()
     return args, parser
 
